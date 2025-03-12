d0c219db29af348027dca5b3f4ad58f8bd6255f6
SamuelP314 <samuelpanartsantolaria@gmail.com>
2025-03-12 12:13:05 +0100

    Sexto commit

--- Cancelar_Reserva.py
+++ Cancelar_Reserva.py
@@ -1,2 +1,26 @@
-def mi_funcion():
-    print("gdehghjkfdh")
+def Cancelar_Reserva(Asientos):
+   
+    asientos_libres = [int(Asiento) for Asiento, Estado in Asientos.items() if Estado["Estado"] == "Libre"]
+   
+    while True:
+        asiento_elegido = int(input("\n¿Cuál es el número del asiento del cual quiere cancelar la reserva? \n"))
+        if asiento_elegido not in asientos_libres and 0 < asiento_elegido <= 10:
+            break
+        elif asiento_elegido in asientos_libres:
+            print("\nSe ha debido de confundir, el asiento introducido está libre. Por favor, intentelo de nuevo.\n")
+        else:
+            print("\nSe ha debido de confundir, el asiento no existe. Por favor, intentelo de nuevo.\n")
+   
+    while True:
+        nombre = input("\nPor motivos de seguridad, necesitamos el nombre al que se puso la reserva. \n")
+        if Asientos[asiento_elegido]["Nombre"] == nombre:
+            Asientos[asiento_elegido]["Estado"] = "Libre"
+            Asientos[asiento_elegido]["Nombre"] = ""
+            asientos_libres.append(asiento_elegido)
+            asientos_libres.sort()
+            print("\nReserva cancelada con éxito.\n")
+            break
+        else:
+            print("\nEl nombre no coincide con la reserva del asiento. Por favor, intentelo de nuevo.\n")
+   
+    return Asientos
